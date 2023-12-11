620f49202999f3345fb005b4735ab397035ec61b
hemant.gogoi <hemant63821@gmail.com>
2023-12-12 02:36:18 +0530

    fix() : removed unnecessary code

--- src/auth/dto/Response.dto.ts
+++ src/auth/dto/Response.dto.ts
@@ -1,19 +1,9 @@
-// export class ErrorResponse {
-//     isSuccessful: boolean = false;
-//     error: string;
-
-// }
-// export class SuccessResponse {
-//     isSuccessful: boolean = true;
-//     result: Array<any> | Record<any, any>;
-//     message: string;
-// }
 
 export class ApiResponse{
     constructor(public isSuccessful: boolean, public data: any, public message: string, ) {}
   }
 
-export class SuccessResponse<T = any> extends ApiResponse {
+export class SuccessResponse extends ApiResponse {
     constructor(data: any, message = 'Success') {
       super(true, data, message);
     }
