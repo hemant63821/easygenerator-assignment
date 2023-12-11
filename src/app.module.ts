aaf7e600e7433b4025fbb16ff1631dc65480fcba
hemant.gogoi <hemant63821@gmail.com>
2023-12-11 23:56:30 +0530

    feature() : installed required packages and db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/easygenerator'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
